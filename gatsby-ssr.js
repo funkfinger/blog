86c76db06c219667dd32de01102fcd4c11203983
Jay Wiggins <jay@irk.com>
2020-03-18 22:03:57 -0700

    fixing gatsby-ssr to be the same as gatsby-browser

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,33 @@
  */
 
 // You can delete this file if you're not using it
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import { MDXProvider } from '@mdx-js/react';
+import Gist from 'react-gist';
+
+import Layout from './src/components/Layout/Layout';
+import CodeHighlighter from './src/components/CodeHighlighter/CodeHighlighter';
+import YouTubeVideo from './src/components/YouTubeVideo/YouTubeVideo';
+
+import './src/style/main.scss';
+
+const components = {
+  code: CodeHighlighter,
+  Gist,
+  YouTubeVideo,
+};
+
+export const wrapPageElement = ({ element, props }) => (
+  <Layout {...props}>{element}</Layout>
+);
+
+export const wrapRootElement = ({ element }) => (
+  <MDXProvider components={{ ...components }}>
+    <HelmetProvider>{element}</HelmetProvider>
+  </MDXProvider>
+);
