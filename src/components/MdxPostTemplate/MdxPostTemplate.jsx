0383f661b3da7ff8ca699f54035d25267d3f1f28
Jay Wiggins <jayw@irk.com>
2021-07-15 21:37:34 -0700

    merge with master

--- src/components/MdxPostTemplate/MdxPostTemplate.jsx
+++ src/components/MdxPostTemplate/MdxPostTemplate.jsx
@@ -1,45 +1,8 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-// import { MDXRenderer } from 'gatsby-plugin-mdx';
-
-// import HeroImage from '../HeroImage/HeroImage';
 
 import PageTemplatePure from './PageTemplatePure';
-
-// const PageTemplatePure = ({ heroImage, title, date, body, obsolete }) => {
-//   const img = heroImage ? (
-//     <HeroImage img={heroImage.childImageSharp.fluid} />
-//   ) : null;
-//   return (
-//     <div className={`single-post${obsolete ? ' obsolete' : ''}`}>
-//       {img}
-//       <div className="single-post-body">
-//         <h1>{title}</h1>
-//         <div className="post-date">{date}</div>
-//         <MDXRenderer>{body}</MDXRenderer>
-//       </div>
-//     </div>
-//   );
-// };
-
-// PageTemplatePure.propTypes = {
-//   heroImage: PropTypes.shape({
-//     childImageSharp: PropTypes.shape({ fluid: PropTypes.shape({}) }),
-//   }),
-//   title: PropTypes.string,
-//   date: PropTypes.string,
-//   body: PropTypes.string,
-//   obsolete: PropTypes.bool,
-// };
-
-// PageTemplatePure.defaultProps = {
-//   heroImage: null,
-//   title: '',
-//   date: '',
-//   body: '',
-//   obsolete: false,
-// };
 
 const PageTemplate = ({
   data: {
